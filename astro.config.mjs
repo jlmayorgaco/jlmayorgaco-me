f9fc649e13aa80198423b2bfb92de34bbbf00bff
Ing. Jorge Luis Mayorga Taborda <167396627+jlmayorgaco@users.noreply.github.com>
2026-03-26 19:35:09 -0500

    Add UI Freatures

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
+
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://jlmayorga.co',
-	integrations: [mdx(), react(), sitemap()],
+	site: 'https://jlmayorga.com.co',
+	integrations: [mdx(), react(), sitemap(), icon()],
 });
